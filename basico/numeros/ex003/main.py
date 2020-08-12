693967ce3b3e70dfe347fdeaffe13be20429654d
Rafael Morán <rm.veiga26@gmail.com>
2020-08-12 16:21:23 -0300

    ADD - nível: básico, categoria: números; exercício: 003

---
+++ basico/numeros/ex003/main.py
@@ -0,0 +1,7 @@
+nota1 = float(input('Informe a 1a nota: '))
+nota2 = float(input('Informe a 2a nota: '))
+nota3 = float(input('Informe a 3a nota: '))
+nota4 = float(input('Informe a 4a nota: '))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f'A média final é {media:.2f}')
